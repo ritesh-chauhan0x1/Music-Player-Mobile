840c11cf43c4eddd59d0fcfda1e2b3b7f80f81c4
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:29 +0530

    Initial commit: Music-Player-Mobile

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Music-Player-Mobile"</h1>
+        <p>"Ionic audio streaming"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
